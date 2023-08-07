7a5268e5fa7cc94a57d713adf1af0e9e4648bd0a
Sibi Prabakaran <sibi@psibi.in>
2023-08-07 10:39:54 +0530

    Add sample build

---
+++ .ci/cloudflare-build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir bin
+curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.34/mdbook-v0.4.34-x86_64-unknown-linux-gnu.tar.gz | tar -xz --directory=bin
+
+bin/mdbook --version
