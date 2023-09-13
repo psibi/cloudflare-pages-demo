6d9ba878b599affc22e3b2ae31a63c18ca7e7cdc
Sibi Prabakaran <sibi@psibi.in>
2023-09-13 17:20:01 +0530

    Install docker

--- .ci/cloudflare-build.sh
+++ .ci/cloudflare-build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+sudo apt update -y
+sudo apt install docker.io -y
+
+
 mkdir bin
 curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.34/mdbook-v0.4.34-x86_64-unknown-linux-gnu.tar.gz | tar -xz --directory=bin
 
