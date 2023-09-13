cd0abd9a09f52bb650d545138bc078d4b35cc426
Sibi Prabakaran <sibi@psibi.in>
2023-09-13 17:22:38 +0530

    Remove sudo

--- .ci/cloudflare-build.sh
+++ .ci/cloudflare-build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt update -y
-sudo apt install docker.io -y
-
+apt update -y
+apt install docker.io -y
 
 mkdir bin
 curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.34/mdbook-v0.4.34-x86_64-unknown-linux-gnu.tar.gz | tar -xz --directory=bin
